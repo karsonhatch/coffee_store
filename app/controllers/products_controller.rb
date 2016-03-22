718a7141200666e6a2a919e4e024e420377bd679
Karson Hatch <karsonhatch@gmail.com>
2016-03-22 14:06:34 -0600

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+class ProductsController < ApplicationController
+  def index
+  end
+
+  def product_card
+  	@product = params[:product]
+  	render(partial: 'product_card')
+  end
+
+  def detail_card 
+  	@product = params[:product]
+  	render(partial: 'product_card')
+  end
+
+end
